aab9664c627a6941dc7ea4b703c0f4c570ad139f
Michael O'Farrell <mofarrell1967@gmail.com>
2020-02-03 15:05:34 +0000

    Added viewtasks.html with initial cards and app.py route settings

--- env.py
+++
@@ -1,3 +0,0 @@
-import os
-os.environ["MONGO_DBNAME"] = 'project-task-tracker'
-os.environ["MONGO_URI"] = 'mongodb+srv://root:Lismara1@myfirstcluster-gpsqs.mongodb.net/project-task-tracker?retryWrites=true&w=majority'
