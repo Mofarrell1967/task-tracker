f09e303d3926eaa7c48c6caadf422aeb793d758d
Michael O'Farrell <mofarrell1967@gmail.com>
2020-02-03 14:57:14 +0000

    Added app.py

--- app.py
+++ app.py
@@ -2,3 +2,23 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = os.environ.get('DATABASE')
+if os.path.exists("env.py"):
+    app.config["MONGO_URI"] = os.environ.get('MONGO_URI')    
+
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def view_tasks():
+    return render_template("viewtasks.html", tasks=mongo.db.tasks.find())
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
--- env.py
+++ env.py
@@ -1,3 +1,3 @@
-app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'project-task-tracker'
-app.config["MONGO_URI"] = 'mongodb+srv://root:Lismara1@myfirstcluster-gpsqs.mongodb.net/project-task-tracker?retryWrites=true&w=majority'
+import os
+os.environ["MONGO_DBNAME"] = 'project-task-tracker'
+os.environ["MONGO_URI"] = 'mongodb+srv://root:Lismara1@myfirstcluster-gpsqs.mongodb.net/project-task-tracker?retryWrites=true&w=majority'
